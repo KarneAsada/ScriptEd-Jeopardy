abf04a2466fd76bf89b8fd6cbcab6056cf54731a
Rob Kovacs <rob@robkovacs.com>
2015-10-28 14:00:43 -0400

    Working on JS

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,28 +1,30 @@
 // https://www.youtube.com/watch?v=BM4GPPhpLYU
 // use bootstrap for columns & rows.
 
-function main(){
-	var teams = {
-		teamOne: {
-			name: "",
-			score: 0
-		},
-		teamTwo: {
-			name: "",
-			score: 0
-		}
-	};
 
-	$('.question').click(function(){
-		var value = $(this).children('.value')[0];
-		var answer = $(this).children('.answer')[0];
-		var points = answer.getAttribute('data-points');
-		$(value).toggle();
-		$(answer).toggle();
-		// console.log('points:', points);
-		$(answer).toggleClass('active');
-	});
-};
+/*global $ */
+$(document).ready(function () {
+    'use strict';
 
+    var teams = {
+        teamOne: {
+            name: "",
+            score: 0
+        },
+        teamTwo: {
+            name: "",
+            score: 0
+        }
+    };
 
-$(document).ready(main);
+    $('.question').on('click', function () {
+        var value = $(this).children('.value')[0];
+        var answer = $(this).children('.answer')[0];
+        var points = answer.getAttribute('data-points');
+        $(value).toggle();
+        $(answer).toggle();
+        $(answer).toggleClass('active');
+        $(this).off('click');
+    });
+
+});
