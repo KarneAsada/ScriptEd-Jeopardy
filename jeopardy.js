ddcb961cb3357aa4d24f7e8251f2719e8214feb6
FantastiCole <coleamurphy@gmail.com>
2015-10-28 11:34:30 -0400

    Begin adding question clicking funtionality.

--- jeopardy.js
+++ jeopardy.js
@@ -1,3 +1,17 @@
 // https://www.youtube.com/watch?v=BM4GPPhpLYU
 // use bootstrap for columns & rows.
 
+function main(){
+
+	$('.question').click(function(){
+		var value = $(this).children('.value')[0];
+		var answer = $(this).children('.answer')[0];
+		value.hide();
+		$(answer).show();
+		console.log('value: ', value);
+		console.log('answer: ', answer);
+	})
+};
+
+
+$(document).ready(main);
