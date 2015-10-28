7df38e23ec6e338ebcf0a597616ef43d5c7d989e
FantastiCole <coleamurphy@gmail.com>
2015-10-28 13:37:06 -0400

    Add pointer on question hover, data-points to html and team object to js.

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -2,14 +2,26 @@
 // use bootstrap for columns & rows.
 
 function main(){
+	var teams = {
+		teamOne: {
+			name: "",
+			score: 0
+		},
+		teamTwo: {
+			name: "",
+			score: 0
+		}
+	};
 
 	$('.question').click(function(){
 		var value = $(this).children('.value')[0];
 		var answer = $(this).children('.answer')[0];
+		var points = answer.getAttribute('data-points');
 		$(value).toggle();
 		$(answer).toggle();
+		// console.log('points:', points);
 		$(answer).toggleClass('active');
-	})
+	});
 };
 
 
